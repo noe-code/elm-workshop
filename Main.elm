6ed358f513028d81012a3469feb02e1a17ed1ca2
Noemi Rodriguez <elmaildenoe@gmail.com>
2016-12-29 20:17:28 +0100

    Part 2

--- Main.elm
+++ Main.elm
@@ -1,14 +1,51 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, href)
+
+
+type alias Result =
+    { id : Int
+    , name : String
+    , stars : Int
+    }
+
+
+model : { result : Result }
+model =
+    { result =
+        { id = 1
+        , name = "TheSeamau5/elm-checkerboardgrid-tutorial"
+        , stars = 66
+        }
+    }
+
+
+viewHeader : Html a
+viewHeader =
+    header []
+        [ h1 [] [ text "ElmHub" ]
+        , span [ class "tagline" ] [ text "Like GitHub, but for Elm things." ]
+        ]
+
+
+viewResults : Html a
+viewResults =
+    ul [ class "results" ]
+        [ li []
+            [ span [ class "star-count" ]
+                [ model.result.stars
+                    |> toString
+                    |> text
+                ]
+            , a [ href ("https://github.com/" ++ model.result.name) ] [ text model.result.name ]
+            ]
+        ]
 
 
 main : Html a
 main =
     div [ class "content" ]
-        [ header []
-            [ h1 [] [ text "ElmHub" ]
-            , span [ class "tagline" ] [ text "Like GitHub, but for Elm things." ]
-            ]
+        [ viewHeader
+        , viewResults
         ]
