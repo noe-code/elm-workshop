4b00d53bdd14508b13314afd148d2ce0e423b839
Noemi Rodriguez <elmaildenoe@gmail.com>
2017-01-02 20:05:24 +0100

    Part 3

--- Main.elm
+++ Main.elm
@@ -1,7 +1,8 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (class, href)
+import Html.Attributes exposing (class, href, target)
+import Html.Events exposing (onClick)
 
 
 type alias Result =
@@ -11,14 +12,58 @@ type alias Result =
     }
 
 
-model : { result : Result }
-model =
-    { result =
-        { id = 1
-        , name = "TheSeamau5/elm-checkerboardgrid-tutorial"
-        , stars = 66
-        }
+type alias Model =
+    { results : List Result }
+
+
+
+-- MODEL
+
+
+initialModel : Model
+initialModel =
+    { results =
+        [ { id = 1
+          , name = "TheSeamau5/elm-checkerboardgrid-tutorial"
+          , stars = 66
+          }
+        , { id = 2
+          , name = "grzegorzbalcerek/elm-by-example"
+          , stars = 41
+          }
+        , { id = 3
+          , name = "sporto/elm-tutorial-app"
+          , stars = 35
+          }
+        , { id = 4
+          , name = "jvoigtlaender/Elm-Tutorium"
+          , stars = 10
+          }
+        , { id = 5
+          , name = "sporto/elm-tutorial-assets"
+          , stars = 7
+          }
+        ]
     }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = DeleteById Int
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        DeleteById id ->
+            { model | results = List.filter (\result -> result.id /= id) model.results }
+
+
+
+-- VIEW
 
 
 viewHeader : Html a
@@ -29,23 +74,40 @@ viewHeader =
         ]
 
 
-viewResults : Html a
-viewResults =
-    ul [ class "results" ]
-        [ li []
-            [ span [ class "star-count" ]
-                [ model.result.stars
-                    |> toString
-                    |> text
-                ]
-            , a [ href ("https://github.com/" ++ model.result.name) ] [ text model.result.name ]
+viewResults : List Result -> Html Msg
+viewResults results =
+    ul [ class "results" ] (List.map viewSearchResults results)
+
+
+viewSearchResults : Result -> Html Msg
+viewSearchResults result =
+    li []
+        [ span [ class "star-count" ]
+            [ result.stars
+                |> toString
+                |> text
             ]
+        , a [ href ("https://github.com/" ++ result.name), target "_blank" ] [ text result.name ]
+        , button [ class "hide-result", onClick (DeleteById result.id) ] [ text "X" ]
         ]
 
 
-main : Html a
-main =
+view : Model -> Html Msg
+view model =
     div [ class "content" ]
         [ viewHeader
-        , viewResults
+        , viewResults model.results
         ]
+
+
+
+-- MAIN
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
